9f44e378055f5f02a162672cbe4d28e1ebdf8ca1
cdelv <cdelv@unal.edu.co>
2023-10-15 09:35:36 -0500

    Reorganization of the Bases directory.

--- Bases/run_all.sh
+++ Bases/run_all.sh
@@ -2,12 +2,18 @@
 set -xo
 I_PATH=$(pwd)
 SIESTA=siesta-4.1.5
-NPROC=2
+NPROC=4
 
 
 for BASE in Adenine Cytosine Guanine Thymine 
 do
-	cd $I_PATH/${BASE}
+	cd $I_PATH/${BASE}/${BASE}_Infinity
+	mpirun -np $NPROC $SIESTA < ${BASE}.fdf > ${BASE}.out
+	cd $I_PATH/${BASE}/${BASE}_Single_Point
+	mpirun -np $NPROC $SIESTA < ${BASE}.fdf > ${BASE}.out
+	cd $I_PATH/${BASE}/Relaxation_Process/Try1
+	mpirun -np $NPROC $SIESTA < ${BASE}.fdf > ${BASE}.out
+	cd $I_PATH/${BASE}/Relaxation_Process/Try2
 	mpirun -np $NPROC $SIESTA < ${BASE}.fdf > ${BASE}.out
 done
 
